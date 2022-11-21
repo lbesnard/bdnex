5b1e7cb3bb2d9f3d90b9cbb697b5da212cda1a18
lbesnard <laurent.besnard@utas.edu.au>
2022-11-21 00:01:10 +0000

    bdgest: fix if key value not available

--- bdnex/lib/bdgest.py
+++ bdnex/lib/bdgest.py
@@ -396,11 +396,13 @@ class BdGestParse():
 
         if isinstance(album_meta_dict['Planches'], str):
             album_meta_dict['Planches'] = int(album_meta_dict['Planches'])
-        if isinstance(album_meta_dict['Tome'], str):
-            regex = re.compile(r'(\d+|\s+)')
-            r = regex.split(album_meta_dict['Tome'])
-            tome = list(filter(None, r))[-1]
-            album_meta_dict['Tome']= int(tome)
+
+        if hasattr(album_meta_dict, 'Tome'):
+            if isinstance(album_meta_dict['Tome'], str):
+                regex = re.compile(r'(\d+|\s+)')
+                r = regex.split(album_meta_dict['Tome'])
+                tome = list(filter(None, r))[-1]
+                album_meta_dict['Tome']= int(tome)
 
         self.album_meta_dict = album_meta_dict
         comicrack_dict = self.comicinfo_metadata(album_meta_dict)
